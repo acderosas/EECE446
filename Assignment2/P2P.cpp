90ee88282b24b7c775c30ff3fdaf74c02c5fad8e
Jake Baartman <jrbaartman@csuchico.edu>
2025-02-26 23:37:23 -0800

    Skeleton

--- Assignment2/P2P.cpp
+++ Assignment2/P2P.cpp
@@ -56,44 +56,7 @@ int main(int argc, char* argv[]) {
     return 0;
 }
 
-// Function definitions - keeping lookup_and_connect exactly as provided
-int lookup_and_connect(const char* host, const char* service) {
-    struct addrinfo hints;
-    struct addrinfo *rp, *result;
-    int s;
-
-    /* Translate host name into peer's IP address */
-    memset(&hints, 0, sizeof(hints));
-    hints.ai_family = AF_UNSPEC;
-    hints.ai_socktype = SOCK_STREAM;
-    hints.ai_flags = 0;
-    hints.ai_protocol = 0;
-
-    if ((s = getaddrinfo(host, service, &hints, &result)) != 0) {
-        fprintf(stderr, "stream-talk-client: getaddrinfo: %s\n", gai_strerror(s));
-        return -1;
-    }
-
-    /* Iterate through the address list and try to connect */
-    for (rp = result; rp != NULL; rp = rp->ai_next) {
-        if ((s = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol)) == -1) {
-            continue;
-        }
-
-        if (connect(s, rp->ai_addr, rp->ai_addrlen) != -1) {
-            break;
-        }
-
-        close(s);
-    }
-    if (rp == NULL) {
-        perror("stream-talk-client: connect");
-        return -1;
-    }
-    freeaddrinfo(result);
-
-    return s;
-}
+// Function definitions 
 
 // Initialize peer
 int initialize_peer(P2PPeer* peer, uint32_t id, const char* reg_host, uint16_t reg_port) {
@@ -136,4 +99,42 @@ void command_loop(P2PPeer* peer) {
 
 // Clean up resources
 void cleanup_peer(P2PPeer* peer) {
+}
+
+int lookup_and_connect(const char* host, const char* service) {
+    struct addrinfo hints;
+    struct addrinfo *rp, *result;
+    int s;
+
+    /* Translate host name into peer's IP address */
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = 0;
+    hints.ai_protocol = 0;
+
+    if ((s = getaddrinfo(host, service, &hints, &result)) != 0) {
+        fprintf(stderr, "stream-talk-client: getaddrinfo: %s\n", gai_strerror(s));
+        return -1;
+    }
+
+    /* Iterate through the address list and try to connect */
+    for (rp = result; rp != NULL; rp = rp->ai_next) {
+        if ((s = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol)) == -1) {
+            continue;
+        }
+
+        if (connect(s, rp->ai_addr, rp->ai_addrlen) != -1) {
+            break;
+        }
+
+        close(s);
+    }
+    if (rp == NULL) {
+        perror("stream-talk-client: connect");
+        return -1;
+    }
+    freeaddrinfo(result);
+
+    return s;
 }
